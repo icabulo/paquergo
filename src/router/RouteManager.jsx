a26e0cbe3a4ea743207ec328750a137f986dd73a
Ivan <icabulo@gmail.com>
2023-06-12 18:03:32 -0500

    feat: dashboard component make modular

--- src/router/RouteManager.jsx
+++ src/router/RouteManager.jsx
@@ -1,4 +1,36 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, UserDashboard } from "../pages";
+import { DevTest } from "../pages/DevTest";
+
+/* const routerConfig = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <div>Hubo un error!!</div>,
+  },
+  {
+    path: "/dashboard",
+    element: <UserDashboard />,
+  },
+  {
+    path: "/devtest",
+    element: <DevTest />,
+  },
+]); */
+
+const CustomRouter = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/dashboard" element={<UserDashboard />}></Route>
+      <Route path="/devtest/*" element={<DevTest />}></Route>
+      <Route path="*" element={<div>404 not found</div>}></Route>
+    </Routes>
+  </BrowserRouter>
+);
+
+export { CustomRouter };
+/* import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home, UserDashboard } from "../pages";
 import { DevTest } from "../pages/DevTest";
 
@@ -22,4 +54,4 @@ const CustomRouter = () => (
   <RouterProvider router={routerConfig}></RouterProvider>
 );
 
-export { CustomRouter };
+export { CustomRouter }; */
